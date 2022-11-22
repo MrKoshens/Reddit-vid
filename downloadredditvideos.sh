eab9c4eb76539815fc5f69c4578666173abf5182
Madhav Verma <madhavvermakeshav@gmail.com>
2022-11-22 11:54:57 +0530

    Update downloadredditvideos.sh

--- downloadredditvideos.sh
+++ downloadredditvideos.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-cd ~/Downloads/videos/
-
-echo -n "search keyword: "
+#enter the subreddit
+echo -n "search Subreddit: "
 
 read answer
 
+#code
 
 youtube-dl -i $(curl -s -H "User-agent: 'your bot 0.1'" https://www.reddit.com/r/$answer/.json\?limit\=100 | jq "." | grep url_overridden_by_dest | grep -Eoh "https:\/\/v\.redd\.it\/\w{13}|https:\/\/youtu\.be\/[a-zA-Z0-9_-]{11}")
 
 j=0;for i in $(ls); do (( j++ ));mv "$i" 000_"$j.mp4"; done
-
 
 j=0
 
@@ -19,4 +18,3 @@ mpv *.mp4
 #comment this line if you want to keep all the videos
 #rm -r *.mp4
 
-cd ~/scripts/
